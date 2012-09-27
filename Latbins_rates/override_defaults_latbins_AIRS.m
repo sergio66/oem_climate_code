909cd46dc867fe5119c8c1f3d72a187d320a0acc
Sergio Machado <sergio@umbc.edu>
2012-09-27 16:38:20 -0400

    Updates : 27-Sep-2012 16:38

--- Latbins_rates/override_defaults_latbins_AIRS.m
+++ Latbins_rates/override_defaults_latbins_AIRS.m
@@ -53,9 +53,9 @@ driver.oem.lambda_temp      = 1;
 driver.jacobian.filename = 'AIRS_MATFILES/all_kcarta_jacs_6_97_97.mat';
 
 %% copy stuff from /home/sergio/MATLABCODE/RATES_TARO/MAT/
-driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Aug28_2012_span_07_2007_07_2012.mat';
-driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep11_2012_span_07_2007_07_2012.mat';
-driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_day_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
+%driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Aug28_2012_span_07_2007_07_2012.mat';
+%driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep11_2012_span_07_2007_07_2012.mat';
+%driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_day_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
 driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
 
 %% orig
