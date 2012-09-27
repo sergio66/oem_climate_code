658fc5175901838cf52eb8bd155562afc254b5dd
Sergio Machado <sergio@umbc.edu>
2012-09-27 14:19:16 -0400

    Updates : 27-Sep-2012 14:19

--- Latbins_rates/override_defaults_latbins_AIRS.m
+++ Latbins_rates/override_defaults_latbins_AIRS.m
@@ -18,6 +18,14 @@ driver.rateset.min = 180;
 driver.rateset.max = +0.25;
 driver.rateset.min = -0.25;
 
+%% orig 6_97_97 stuff
+driver.jacobian.filename = 'M_TS_jac_all.mat';
+driver.jacobian.qstnames = {'CO2' 'O3' 'N2O' 'CH4' 'CFC11' 'stemp'};
+driver.jacobian.qstYesOrNo = [1     1   1     1      1       1];
+driver.jacobian.numQlays   = 1;   %% how many gases we want to retrieve profiles
+                                  %% must be at least 1 (for water)
+driver.jacobian.numlays     = 97;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% larrabee used these in the file he gave me, but I get bad trace gas rates
 %%% they give decent WV/T rates
@@ -41,21 +49,16 @@ driver.oem.lambda_temp      = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% orig 6_97_97 stuff
-driver.jacobian.filename = 'M_TS_jac_all.mat';
-driver.jacobian.qstnames = {'CO2' 'O3' 'N2O' 'CH4' 'CFC11' 'stemp'};
-driver.jacobian.qstYesOrNo = [1     1   1     1      1       1];
-driver.jacobian.numQlays   = 1;   %% how many gases we want to retrieve profiles
-                                  %% must be at least 1 (for water)
-driver.jacobian.numlays     = 97;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%
-
 driver.rateset.datafile = '/home/sergio/MATLABCODE/RATES_TARO/MAT/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Aug28_2012_span_07_2007_07_2012.mat';
 driver.rateset.datafile = '/home/sergio/MATLABCODE/RATES_TARO/MAT/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep11_2012_span_07_2007_07_2012.mat';
 driver.jacobian.filename    = '/home/sergio/MATLABCODE/BUFFER_Fit_pkg/Aux_jacs_AIRS_General/JACS/Sep11_2012/all_kcarta_jacs_6_97_97.mat';
 driver.rateset.datafile = '/home/sergio/MATLABCODE/RATES_TARO/MAT/overocean_gsx_1day_clr_era_lays_spanday01_day_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
 driver.rateset.datafile = '/home/sergio/MATLABCODE/RATES_TARO/MAT/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
+
+%% orig
+driver.oem.lambda_qst       = 0.1;
+driver.oem.lambda_Q1        = 100;
+driver.oem.lambda_temp      = 50;
 
 %% only make diags
 driver.oem.lambda_qst       = [ones(1,6)]*0.1;
@@ -66,11 +69,6 @@ driver.oem.lambda_temp      = [ones(1,10)*1 ones(1,30)*1 ones(1,57)*1]*50;
 driver.oem.lambda_qst       = [10 10 10 10 10 10                              ones(1,1)*(-9999)]*15;
 driver.oem.lambda_Q1        = [ones(1,10)*10.0 ones(1,30)*5.0 ones(1,57)*0.05 ones(1,1)*(-9999)]*1000;
 driver.oem.lambda_temp      = [ones(1,10)*10.0 ones(1,30)*5.0 ones(1,57)*0.05 ones(1,1)*(-9999)]*500;
-
-%% orig
-driver.oem.lambda_qst       = 0.1;
-driver.oem.lambda_Q1        = 100;
-driver.oem.lambda_temp      = 50;
 
 %% yuk
 driver.oem.lambda_qst       = 0.1;
