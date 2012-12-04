8827733d8b18355477225223b79387c1de711715
Sergio Machado <sergio@umbc.edu>
2012-12-04 12:32:02 -0500

    SSM:initial files for era_AIRS_data branch

--- Latbins_rates/JUNK/override_defaults_latbins_AIRS.m
+++ Latbins_rates/JUNK/override_defaults_latbins_AIRS.m
@@ -18,6 +18,7 @@ driver.rateset.min = 180;
 driver.rateset.max = +0.25;
 driver.rateset.min = -0.25;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % orig 6_97_97 stuff
 driver.rateset.ncfile    = 'all_lagcor.mat';
 driver.jacobian.filename = 'M_TS_jac_all.mat';
@@ -27,65 +28,28 @@ driver.jacobian.numQlays   = 1;   % how many gases we want to retrieve profiles
                                   % must be at least 1 (for water)
 driver.jacobian.numlays     = 97;
 
-%########################################################################
-% larrabee used these in the file he gave me, but I get bad trace gas rates
-% they give decent WV/T rates
-%driver.oem.cov_filename     = 'cov_lls.mat'
-%driver.oem.diag_only        = 0;
-%driver.oem.lambda           = 1;
-%driver.oem.lambda_qst       = 0.1;
-%driver.oem.lambda_Q1        = 10;
-%driver.oem.lambda_temp      = 1;
-% ABOVE IS STROW, BELOW IS MINE %%%%%%%%%%%
-% ABOVE IS STROW, BELOW IS MINE %%%%%%%%%%%
+% TEST the SYNTHETIC CASE
+% a) diag case, no damping at all .. get back tracegas rates, and biases are zeroish
+%driver.rateset.datafile  = '../MakeERA_ratespectra/syntheticERArates.mat';
+%driver.oem.adjust_spectral_errorbars = 0.001;
+%driver.oem.lambda           = 1e-11;
+%driver.oem.diag_only        = 1;
 
-% copied rates from /home/sergio/MATLABCODE/RATES_TARO/MAT/
-% driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Aug28_2012_span_07_2007_07_2012.mat';
-% driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep11_2012_span_07_2007_07_2012.mat';
-% driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_day_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
-% driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Sep17_2012_span_09_2002_08_2012.mat';
-
-% good!!!!%%%
-driver.rateset.datafile  = 'AIRS_MATFILES/overocean_gsx_1day_clr_era_lays_spanday01_avgL1Brates_robust_Nov02_2012_span_09_2002_08_2012.mat';
-% good!!!!%%%
-
-% orig
-%driver.oem.lambda_qst       = 0.1;
-%driver.oem.lambda_Q1        = 100;
-%driver.oem.lambda_temp      = 50;
-
-% only make ** diags **
-%driver.oem.lambda_qst       = [ones(1,6)]*0.1;
-%driver.oem.lambda_Q1        = [ones(1,10) ones(1,30) ones(1,57)]*100;
-%driver.oem.lambda_temp      = [ones(1,10)*1 ones(1,30)*1 ones(1,57)*1]*50;
-
-% can individually ** tweak blocks ** [and thus duplicate driver.oem.lambda_temp = Z]
-%driver.oem.lambda_qst       = [10 10 10 10 10 10                              ones(1,1)*(-9999)]*15;
-%driver.oem.lambda_Q1        = [ones(1,10)*10.0 ones(1,30)*5.0 ones(1,57)*0.05 ones(1,1)*(-9999)]*1000;
-%driver.oem.lambda_temp      = [ones(1,10)*10.0 ones(1,30)*5.0 ones(1,57)*0.05 ones(1,1)*(-9999)]*500;
-
-% WORKS WELL WITH Nov02_2012_span_09_2002_08_2012.mat
-driver.oem.lambda_qst       = 0.1;
-driver.oem.lambda_Q1        = 1;
-driver.oem.lambda_temp      = 5;
-driver.oem.lambda_qst       = [0.1 0.1 0.1 0.1 0.10 0.1]*1e-1;
-driver.oem.lambda           = 1e-4;
-driver.oem.adjust_spectral_errorbars = 0.5;
-% WORKS WELL WITH Nov02_2012_span_09_2002_08_2012.mat
-
+% b) some damping, and gives perfect retrievals
+driver.rateset.datafile  = '../MakeERA_ratespectra/syntheticERArates.mat';
+driver.oem.adjust_spectral_errorbars = 0.001;
+driver.oem.lambda           = 1e-11;
+driver.oem.diag_only        = 1;
 driver.oem.diag_only        = false;
 driver.oem.lambda_Q1        = 0.1;
-driver.oem.lambda_temp      = 0.01;
+driver.oem.lambda_temp      = 0.001;
 driver.oem.lambda_qst       = [0.1 0.1 0.1 0.1 0.10 0.1]*1e-2;
-driver.oem.lambda           = 1e-2;
-
-driver.oem.diag_only        = false;
-driver.oem.lambda_Q1        = 5;
-driver.oem.lambda_temp      = 1;
-driver.oem.lambda_qst       = [0.1 0.1 0.1 0.1 0.10 0.1]*1e-2;
-driver.oem.lambda           = 1e+2;
-
+driver.oem.lambda           = 1e-1;
 driver.oem.spectralcov_filename = ['../MakeERA_ratespectra/cov_spectra_yy_2009_latbin_' num2str(ix,'%02d') '.mat'];
+% TEST the SYNTHETIC CASE
 
 fprintf(1,'%s \n',driver.rateset.datafile);
 
+% g = dogoodchan;
+% driver.jacobian.chanset = g;
+% driver.jacobian.chanset = driver.jacobian.chanset(1:50:end);
