2f5808258d5031c1771e62c4490d6fef864fef04
Andrew Tangborn <tangborn@tara-fe1.rs.umbc.edu>
2013-08-20 10:57:47 -0400

    Update to error covariance to improve strat "fits". Look at wavenumbers < 700cm-1

--- Tangborn/override_defaults_latbins_AIRS.m
+++ Tangborn/override_defaults_latbins_AIRS.m
@@ -60,7 +60,7 @@ driver.jacobian.filename = 'M_TS_jac_all.mat';
   %% so eg Andy Tangborn finds after normalization driver.oem.sigma.temp_strat_VALUE  = 4;
   %% is a "good value" which means we default set it to 4*0.01 (0.01 is qrenorm for T(z))
   %% strat temp
-    driver.oem.sigma.temp_strat_VALUE  = 2*0.01;       %% sigsqr
+    driver.oem.sigma.temp_strat_VALUE  = 1*0.01;       %% sigsqr
     driver.oem.sigma.temp_strat_TOPLAY = 01;  %% start layer
     driver.oem.sigma.temp_strat_BOTLAY = 49;  %% stop layer
   %% upper trop temp 
@@ -74,17 +74,17 @@ driver.jacobian.filename = 'M_TS_jac_all.mat';
     driver.oem.sigma.temp_trop_TOPLAY = driver.oem.sigma.temp_upper_trop_BOTLAY + 1; %% start layer
     driver.oem.sigma.temp_trop_BOTLAY = driver.jacobian.numlays;                %% stop layer
   %% strat GAS 1
-    driver.oem.sigma.hum_strat_VALUE  = 0.6*0.01;       %% sigsqr
+    driver.oem.sigma.hum_strat_VALUE  = 0.7*0.01;       %% sigsqr
     driver.oem.sigma.hum_strat_TOPLAY = 01;  %% start layer
     driver.oem.sigma.hum_strat_BOTLAY = 49;  %% stop layer
   %% trop GAS 1
-    driver.oem.sigma.hum_trop_VALUE  = 0.1*0.01;       %% sigsqr
+    driver.oem.sigma.hum_trop_VALUE  = 0.08*0.01;       %% sigsqr
     driver.oem.sigma.hum_trop_TOPLAY = 50;  %% start layer
     driver.oem.sigma.hum_trop_BOTLAY = 97;  %% stop layer
     driver.oem.sigma.hum_trop_TOPLAY = driver.oem.sigma.hum_strat_BOTLAY + 1; %% start layer
     driver.oem.sigma.hum_trop_BOTLAY = driver.jacobian.numlays;                %% stop layer
   %% length_correlation for T(z) and WV(z) .. note this is in terms of INDEX
-    driver.oem.sigma.l_c = 2.4;
+    driver.oem.sigma.l_c = 2.3;
   %% QST 1 ..6 values
     driver.oem.sigma.qst(1) = .008*2.20;  %% co2
     driver.oem.sigma.qst(2) = 1*0.01;  %% o3
