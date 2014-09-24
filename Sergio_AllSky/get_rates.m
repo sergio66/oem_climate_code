70e9878240fc26b5336d9335ee7b5197f05bf670
Sergio Machado <sergio@umbc.edu>
2014-09-24 12:03:38 -0400

    SSM:added new dir for allsky (Sergio_AllSky)

---
+++ Sergio_AllSky/get_rates.m
@@ -0,0 +1,17 @@
+function driver = get_rates(driver)
+
+ix = driver.iibin;
+load(driver.rateset.datafile)
+switch driver.rateset.ocb_set
+  case 'bias'
+     driver.rateset.rates = real(squeeze(b_bias(ix,:,2))');
+     driver.rateset.unc_rates = real(squeeze(b_err_bias(ix,:,2))');
+  case 'cal'
+     driver.rateset.rates = real(squeeze(b_cal(ix,:,2))');
+     driver.rateset.unc_rates = real(squeeze(b_err_cal(ix,:,2))');
+  case {'obs','tracegas'}
+     driver.rateset.rates = real(squeeze(b_obs(ix,:,2))');
+     driver.rateset.unc_rates = real(squeeze(b_err_obs(ix,:,2))');
+end
+%driver.rateset.r = r(:,ix);
+
---
+++ Sergio_AllSky/run_retrieval_latbins_AIRS.m
@@ -0,0 +1,62 @@
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+% run_retrieval_latbins_AIRS.m
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+% Select the latitude bin
+driver.iibin = JOB;
+%---------------------------------------------------------------------------
+% Need oem_pkg
+addpath ../../oem_pkg
+%---------------------------------------------------------------------------
+% Doing debug?
+driver.debug = false;
+driver.debug_dir = '../Debug';
+
+% Open debug file if desired
+if driver.debug
+  writelog('open');
+end;
+%---------------------------------------------------------------------------
+% Perform OEM fit?
+driver.oem.dofit = true;
+driver.lls.dofit = false;
+
+% Oem loops?  Just one if linear.
+driver.oem.nloop = 1;
+
+% Debug plots inside rodgers?
+driver.oem.doplots = false;
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+% Override many settings and add covariance matrix
+[driver,aux] = strow_override_defaults_latbins_AIRS(driver);
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+% Do the retrieval
+driver = retrieval(driver,aux);
+%---------------------------------------------------------------------------
+% Save retrieval output
+driver.filename = ['Output/test' int2str(driver.iibin)];
+%save(driver.filename,'-struct','driver');
+%---------------------------------------------------------------------------
+% Close debug file
+if driver.debug
+  writelog('close')
+end
+%---------------------------------------------------------------------------
+% Some simple output
+fprintf('Scalar Retrievals from OEM\n')
+fprintf(1,'CO2   (ppm)   %5.3f  +- %5.3f \n',driver.oem.finalrates(1),driver.oem.finalsigs(1));
+fprintf(1,'O3    (%%)     %5.3f  +- %5.3f \n',100*driver.oem.finalrates(2),100*driver.oem.finalsigs(2));
+fprintf(1,'N2O   (ppb)   %5.3f  +- %5.3f \n',driver.oem.finalrates(3),driver.oem.finalsigs(3));
+fprintf(1,'CH4   (ppb)   %5.3f  +- %5.3f \n',driver.oem.finalrates(4),driver.oem.finalsigs(4));
+fprintf(1,'CFC11 (ppt)  %5.3f  +- %5.3f \n',driver.oem.finalrates(5),driver.oem.finalsigs(5));
+fprintf(1,'SST   (K)    %5.3f  +- %5.3f \n',driver.oem.finalrates(6),driver.oem.finalsigs(6));
+%---------------------------------------------------------------------------
+% Plot Results
+addpath Plotutils
+plot_retrieval_latbins
+%---------------------------------------------------------------------------
+co2(JOB) = driver.oem.finalrates(1);
+
